2ae758ad7d92cdd14cff193ac37dfc5049f4e2ba
santhoshkumar0918 <krishnanramalingam87@gmail.com>
2024-12-21 20:51:33 +0500

    updated file strut

---
+++ client/app/api/unfollow/route.ts
@@ -0,0 +1,22 @@
+import { supabase } from "@/app/lib/supabase";
+import { ifError } from "assert";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { follower_id, following_id } = req.body;
+
+  const { error } = await supabase
+    .from("followers")
+    .delete()
+    .eq("follower_id", follower_id)
+    .eq("following_id", following_id);
+
+  if (error) {
+    return res.status(400).json({ error: error.message });
+  }
+
+  res.status(200).json({ message: "Unfollowed Sucessfully" });
+}
--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import ResponsiveNav from "@/components/Header/Responsivenav";
+import ResponsiveNav from "@/app/components/Header/Responsivenav";
 import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
