40b397afee8de5099f6680d4556a7a463f017d8b
santhoshkumar0918 <krishnanramalingam87@gmail.com>
2024-12-23 18:43:37 +0500

    updated

--- client/app/lib/clerk.ts
+++ client/app/lib/clerk.ts
@@ -2,13 +2,14 @@ import { clerkClient } from "@clerk/nextjs/server";
 import { supabase } from "./supabase";
 
 export const syncClerkUserToSupabase = async (clerkUserId: string) => {
-  const user = await clerkClient.users.getUser(clerkUserId);
+  const client = await clerkClient();
+  const user = await client.users.getUser(clerkUserId);
   const { error } = await supabase.from("users").upsert(
     {
       clerk_id: user.id,
       email: user.emailAddresses[0].emailAddress,
     },
-    { onConflict: ["clerk_id"] }
+    { onConflict: "clerk_id" }
   );
 
   if (error) {
