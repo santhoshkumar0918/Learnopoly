cded44047478901131427bf75139a02bb1aa168c
santhoshkumar0918 <krishnanramalingam87@gmail.com>
2024-12-28 21:13:19 +0500

    added

--- client/app/Providers.tsx
+++ client/app/Providers.tsx
@@ -1,3 +1,34 @@
+// "use client";
+
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import { useState } from "react";
+// import ResponsiveNav from "./components/Header/Responsivenav";
+
+// export default function ClientProviders({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   const [queryClient] = useState(
+//     () =>
+//       new QueryClient({
+//         defaultOptions: {
+//           queries: {
+//             staleTime: 60 * 1000,
+//             refetchOnWindowFocus: false,
+//           },
+//         },
+//       })
+//   );
+
+//   return (
+//     <QueryClientProvider client={queryClient}>
+//       <ResponsiveNav />
+//       <main>{children}</main>
+//     </QueryClientProvider>
+//   );
+// }
+
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -14,7 +45,7 @@ export default function ClientProviders({
       new QueryClient({
         defaultOptions: {
           queries: {
-            staleTime: 60 * 1000, 
+            staleTime: 60 * 1000,
             refetchOnWindowFocus: false,
           },
         },
@@ -23,8 +54,10 @@ export default function ClientProviders({
 
   return (
     <QueryClientProvider client={queryClient}>
-      <ResponsiveNav />
-      <main>{children}</main>
+      <div className="flex">
+        <ResponsiveNav />
+        <main className="ml-36 w-full">{children}</main>
+      </div>
     </QueryClientProvider>
   );
 }
