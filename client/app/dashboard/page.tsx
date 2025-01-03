ac579e917c6e42b186df48ddc2fc18aaaaccf8f8
santhoshkumar0918 <krishnanramalingam87@gmail.com>
2025-01-03 13:31:58 +0500

    removed the wrapper

--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -1,19 +1,19 @@
-"use client"
-import Feed from "../components/Feed/Feed";
-import { useUser } from "@clerk/nextjs";
+// "use client"
+// import Feed from "../components/Feed/Feed";
+// import { useUser } from "@clerk/nextjs";
 
-export default function Dashboard() {
-  const { user, isLoaded } = useUser();
+// export default function Dashboard() {
+  // const { user, isLoaded } = useUser();
 
-  return (
-    <div>
+//   return (
+//     <div>
       
-      <div className="w-full  bg-blue-200 mx-auto ">
-        <h1 className="text-3xl font-bold mb-4">
-          Welcome, {isLoaded && user ? user.firstName : "Guest"}
-        </h1>
-        <Feed />
-      </div>
-    </div>
-  );
- }
+//       <div className="w-full  bg-blue-200 mx-auto ">
+//         <h1 className="text-3xl font-bold mb-4">
+//           Welcome, {isLoaded && user ? user.firstName : "Guest"}
+//         </h1>
+//         <Feed />
+//       </div>
+//     </div>
+//   );
+//  }
